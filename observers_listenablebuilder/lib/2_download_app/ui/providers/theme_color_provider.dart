1e8de475367156126c552478ad89bc6cb3cc920c
Samnang-Vouen <vouensamnang@gmail.com>
2026-02-17 13:35:12 +0700

    TASK-002 – Downloader – Color Theme

--- observers_listenablebuilder/lib/2_download_app/ui/providers/theme_color_provider.dart
+++ observers_listenablebuilder/lib/2_download_app/ui/providers/theme_color_provider.dart
@@ -11,4 +11,15 @@ enum ThemeColor {
   Color get backgroundColor => color.withAlpha(100);
 }
 
-ThemeColor currentThemeColor = ThemeColor.blue;
+class ThemeColorProvider extends ChangeNotifier {
+  ThemeColor _currentThemeColor = ThemeColor.blue;
+
+  ThemeColor get currentThemeColor => _currentThemeColor;
+
+  void changeTheme(ThemeColor newTheme) {
+    _currentThemeColor = newTheme;
+    notifyListeners();
+  }
+}
+
+final themeProvider = ThemeColorProvider();
